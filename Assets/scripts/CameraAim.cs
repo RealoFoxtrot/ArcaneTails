221c7b249d7318f7a547ae0110ae28360aee0afb
RealoFoxtrot <darkhollybob@gmail.com>
2016-02-20 19:02:33 +0000

    Importing Test version
    Project Currently Broken. Should have all versions swapped with Tony's

---
+++ Assets/scripts/CameraAim.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAim : MonoBehaviour {
+
+    public Camera playerCamera;
+    public Vector3 hitTransform;
+    public GameObject hitCube;
+    public GameObject Player;
+	// Use this for initialization
+	void Start () {
+
+        playerCamera = GetComponent<Camera>();
+        hitTransform = new Vector3();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Vector3 Worldhit = playerCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, playerCamera.farClipPlane));
+        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (hit.transform.gameObject.tag == "Floor")
+            {
+
+                hitTransform = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+
+            }
+
+
+        }
+
+       // make the object appear at the hit point.
+        hitCube.transform.position = hitTransform;
+      
+
+    }
+}
---
+++ Assets/scripts/PinballMovement.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PinballMovement : MonoBehaviour
+{
+
+    public float speed = 15f;
+    public bool beenhit = false;
+    public float boomForce = 10f;
+    public float boomRadius = 1f;
+
+    private Rigidbody rb;
+    private float horizontal;
+    private float vertical;
+    private Vector3 movement;
+    private Vector3 crossHair;
+    private Vector3 pointAtCrossHair;
+    private Vector3 boomPosition;
+    private float boomMultiplier;
+
+
+    private float timer = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        //horizontal = Input.GetAxis("PS4_Horizontal");
+        //vertical = Input.GetAxis("PS4_Vertical");
+        boomPosition = GameObject.Find("AttackerEnemy").transform.position;
+        boomMultiplier = boomForce * 1000;
+
+        if (GameObject.Find("Attacker").GetComponent<AttackingPoint>().inRange == true && GameObject.Find("Attacker").GetComponent<AttackingPoint>().blastSpellFired == true)
+        {
+            beenhit = true;
+        }
+
+
+
+        // swap between world control and physics pinball
+        if (beenhit == true)
+        {
+            timer += 1.0f * Time.deltaTime;
+            pinballhit();
+        }
+        else
+        {
+            //pinballcontrol(horizontal, vertical);
+            timer = 0;
+            physicsPinballControl();
+        }
+
+        if (timer >= 2)
+        {
+
+            beenhit = false;
+        }
+
+    }
+
+    //Disable the controls and start spinning the player
+    void pinballhit()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.AddExplosionForce(boomMultiplier, boomPosition, boomRadius, 0.1f);
+    }
+
+    //World Co-Ordinate Controlled Movement
+    void pinballcontrol(float horizontal, float vertical)
+    {
+        // Set the movement vector based on the axis input.
+        movement.Set(horizontal, 0f, vertical);
+
+        // Normalise the movement vector and make it proportional to the speed per second.
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Move the player to it's current position plus the movement.
+        rb.MovePosition(transform.position + movement);
+
+        //turn the player towards the crosshair
+        turning();
+    }
+
+    //Tony's physics based movement converted to Elina's game
+    void physicsPinballControl()
+    {
+        if (horizontal > 0 || horizontal < 0)
+        {
+
+            rb.AddForce(horizontal * speed * 1000 * Time.deltaTime, 0, 0);
+
+        }
+
+        if (vertical > 0 || vertical < 0)
+        {
+
+
+            rb.AddForce(0, 0, vertical * speed * 1000 * Time.deltaTime);
+
+        }
+
+        turning();
+    }
+
+
+    void turning()
+    {
+        //get the location of the crosshair from the script in the camera
+        crossHair = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraAim>().hitTransform;
+
+        //create a vector between the crosshair and the player
+        pointAtCrossHair = crossHair - transform.position;
+        //pointAtCrossHair.y = 0.3f;
+
+        Quaternion turnPlayer = Quaternion.LookRotation(pointAtCrossHair);
+
+        rb.MoveRotation(turnPlayer);
+    }
+
+}
